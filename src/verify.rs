7378db4bf54cf2dd4938b8e72044c8811e8dff0f
Attila Oláh <attilaolah@gmail.com>
2023-09-20 23:56:04 +0200

    Fix GMP build issue with Nix Bazel + Nix Clang.
    For now this just makes sure the proto and rug can both build.

--- src/verify.rs
+++ src/verify.rs
@@ -1,3 +1,8 @@
-pub fn verify() -> bool {
+use pratt_rs::Certificate;
+use rug::{integer::Order, Integer};
+
+pub fn verify(cert: Certificate) -> bool {
+    let prime = Integer::from_digits(&cert.prime, Order::Msf);
+
     todo!();
 }
