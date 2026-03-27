d90db47fbd8c292d0c000c4fc402720d03e10f2e
Attila Oláh <attila@dorn.haus>
2026-03-27 21:41:00 +0100

    chore: format with alejandra

--- flake.nix
+++ flake.nix
@@ -5,73 +5,70 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in
-    {
-      packages = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          verifyPkg = pkgs.stdenv.mkDerivation {
-            pname = "verify";
-            version = "0.1.0";
-            src = self;
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+  in {
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+      verifyPkg = pkgs.stdenv.mkDerivation {
+        pname = "verify";
+        version = "0.1.0";
+        src = self;
 
-            nativeBuildInputs = [
-              pkgs.zig
-              pkgs.pkg-config
-            ];
-            buildInputs = [ pkgs.gmp ];
+        nativeBuildInputs = [
+          pkgs.zig
+          pkgs.pkg-config
+        ];
+        buildInputs = [pkgs.gmp];
 
-            dontConfigure = true;
+        dontConfigure = true;
 
-            buildPhase = ''
-              runHook preBuild
-              export ZIG_GLOBAL_CACHE_DIR="$TMPDIR/zig-global-cache"
-              export ZIG_LOCAL_CACHE_DIR="$TMPDIR/zig-local-cache"
-              zig build-exe src/verify_gmp.zig \
-                -O ReleaseSafe \
-                -I ${pkgs.gmp.dev}/include \
-                -L ${pkgs.gmp.out}/lib \
-                -rpath ${pkgs.gmp.out}/lib \
-                -lc -lgmp \
-                -femit-bin=verify
-              runHook postBuild
-            '';
+        buildPhase = ''
+          runHook preBuild
+          export ZIG_GLOBAL_CACHE_DIR="$TMPDIR/zig-global-cache"
+          export ZIG_LOCAL_CACHE_DIR="$TMPDIR/zig-local-cache"
+          zig build-exe src/verify_gmp.zig \
+            -O ReleaseSafe \
+            -I ${pkgs.gmp.dev}/include \
+            -L ${pkgs.gmp.out}/lib \
+            -rpath ${pkgs.gmp.out}/lib \
+            -lc -lgmp \
+            -femit-bin=verify
+          runHook postBuild
+        '';
 
-            installPhase = ''
-              runHook preInstall
-              mkdir -p "$out/bin"
-              install -m755 verify "$out/bin/verify"
-              runHook postInstall
-            '';
-          };
-        in
-        {
-          verify = verifyPkg;
-          default = verifyPkg;
-        });
+        installPhase = ''
+          runHook preInstall
+          mkdir -p "$out/bin"
+          install -m755 verify "$out/bin/verify"
+          runHook postInstall
+        '';
+      };
+    in {
+      verify = verifyPkg;
+      default = verifyPkg;
+    });
 
-      apps = forAllSystems (system:
-        let
-          verifyProgram = "${self.packages.${system}.verify}/bin/verify";
-        in
-        {
-          verify = {
-            type = "app";
-            program = verifyProgram;
-          };
-          default = {
-            type = "app";
-            program = verifyProgram;
-          };
-        });
-    };
+    apps = forAllSystems (system: let
+      verifyProgram = "${self.packages.${system}.verify}/bin/verify";
+    in {
+      verify = {
+        type = "app";
+        program = verifyProgram;
+      };
+      default = {
+        type = "app";
+        program = verifyProgram;
+      };
+    });
+  };
 }
