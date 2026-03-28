767a21cf7fa9158fc79b9318858d9813af03b944
Attila Oláh <attila@dorn.haus>
2026-03-28 23:34:50 +0100

    refactor: use flake-parts

--- flake.nix
+++ flake.nix
@@ -2,99 +2,93 @@
   description = "Pratt certificate verifier";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
+  outputs = inputs @ {
+    flake-parts,
     nixpkgs,
-  }: let
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-  in {
-    packages = forAllSystems (system: let
-      pkgs = import nixpkgs {inherit system;};
-      verifyPkg = pkgs.stdenv.mkDerivation {
-        pname = "verify";
-        version = "0.1.0";
-        src = let
-          inherit (nixpkgs.lib) fileset;
-        in
-          fileset.toSource {
-            root = ./.;
-            fileset = fileset.unions [./src];
-          };
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-        nativeBuildInputs = [
-          pkgs.zig
-          pkgs.pkg-config
-        ];
-        buildInputs = [pkgs.gmp];
+      perSystem = {pkgs, ...}: let
+        verifyPkg = pkgs.stdenv.mkDerivation {
+          pname = "verify";
+          version = "0.1.0";
+          src = let
+            inherit (nixpkgs.lib) fileset;
+          in
+            fileset.toSource {
+              root = ./.;
+              fileset = fileset.unions [./src];
+            };
 
-        dontConfigure = true;
+          nativeBuildInputs = with pkgs; [zig pkg-config];
+          buildInputs = with pkgs; [gmp];
 
-        buildPhase = ''
-          runHook preBuild
-          export ZIG_GLOBAL_CACHE_DIR="$TMPDIR/zig-global-cache"
-          export ZIG_LOCAL_CACHE_DIR="$TMPDIR/zig-local-cache"
-          zig build-exe src/verify.zig \
-            -O ReleaseSafe \
-            -I ${pkgs.gmp.dev}/include \
-            -L ${pkgs.gmp.out}/lib \
-            -rpath ${pkgs.gmp.out}/lib \
-            -lc -lgmp \
-            -femit-bin=verify
-          runHook postBuild
-        '';
+          dontConfigure = true;
 
-        installPhase = ''
-          runHook preInstall
-          mkdir -p "$out/bin"
-          install -m755 verify "$out/bin/verify"
-          runHook postInstall
-        '';
+          buildPhase = ''
+            runHook preBuild
+            export ZIG_GLOBAL_CACHE_DIR="$TMPDIR/zig-global-cache"
+            export ZIG_LOCAL_CACHE_DIR="$TMPDIR/zig-local-cache"
+            zig build-exe src/verify.zig \
+              -O ReleaseSafe \
+              -I ${pkgs.gmp.dev}/include \
+              -L ${pkgs.gmp.out}/lib \
+              -rpath ${pkgs.gmp.out}/lib \
+              -lc -lgmp \
+              -femit-bin=verify
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p "$out/bin"
+            install -m755 verify "$out/bin/verify"
+            runHook postInstall
+          '';
+        };
+        program = pkgs.lib.getExe verifyPkg;
+        verify = {
+          inherit program;
+          type = "app";
+        };
+      in {
+        packages = {
+          verify = verifyPkg;
+          default = verifyPkg;
+        };
+
+        apps = {
+          inherit verify;
+          default = verify;
+        };
+
+        checks = {
+          verify-small = pkgs.runCommand "verify-small" {} ''
+            id=$(printf "3" | sha256sum | cut -c1-16)
+
+            mkdir -p data
+            cat > "data/$id" <<EOF
+            V 1
+            P 3
+            W 2
+            F 2
+            EOF
+
+            ${verifyPkg}/bin/verify "data/$id"
+            touch "$out"
+          '';
+        };
       };
-    in {
-      verify = verifyPkg;
-      default = verifyPkg;
-    });
-
-    apps = forAllSystems (system: let
-      verifyProgram = "${self.packages.${system}.verify}/bin/verify";
-    in {
-      verify = {
-        type = "app";
-        program = verifyProgram;
-      };
-      default = {
-        type = "app";
-        program = verifyProgram;
-      };
-    });
-
-    checks = forAllSystems (system: let
-      pkgs = import nixpkgs {inherit system;};
-      verify = self.packages.${system}.verify;
-    in {
-      verify-small = pkgs.runCommand "verify-small" {} ''
-        id=$(printf "3" | sha256sum | cut -c1-16)
-
-        mkdir -p data
-        cat > "data/$id" <<EOF
-        V 1
-        P 3
-        W 2
-        F 2
-        EOF
-
-        ${verify}/bin/verify "data/$id"
-        touch "$out"
-      '';
-    });
-  };
+    };
 }
